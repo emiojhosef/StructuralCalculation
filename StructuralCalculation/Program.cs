ec5635e9b65fb02e01146335cf3c98adcf415962
Emio Jhosef <emiojhs@gmail.com>
2026-06-12 17:31:36 -0300

    Add circular tube section

--- StructuralCalculation/Program.cs
+++ StructuralCalculation/Program.cs
@@ -1,87 +1,17 @@
-﻿using StructuralCalculation.Sections.CircularSections;
-using StructuralCalculation.Sections.Polygons;
+﻿using StructuralCalculation.Sections.HollowSections;
 
-Console.WriteLine("=== RECTANGLE ===");
-Rectangle rectangle = new Rectangle(10, 4, 0, 0);
-
-Console.WriteLine($"Area = {rectangle.Area()}");
-Console.WriteLine($"Cx = {rectangle.CentroidX()}");
-Console.WriteLine($"Cy = {rectangle.CentroidY()}");
-Console.WriteLine($"Ix = {rectangle.MomentOfInertiaX()}");
-Console.WriteLine($"Iy = {rectangle.MomentOfInertiaY()}");
-Console.WriteLine($"Ixy = {rectangle.ProductOfInertia()}");
-
-Console.WriteLine("\n=== TRIANGLE ===");
-Triangle triangle = new Triangle(6, 5, 5, 0, 0);
-
-Console.WriteLine($"Area = {triangle.Area()}");
-Console.WriteLine($"Cx = {triangle.CentroidX()}");
-Console.WriteLine($"Cy = {triangle.CentroidY()}");
-Console.WriteLine($"Ix = {triangle.MomentOfInertiaX()}");
-Console.WriteLine($"Iy = {triangle.MomentOfInertiaY()}");
-Console.WriteLine($"Ixy = {triangle.ProductOfInertia()}");
-
-Console.WriteLine("\n=== ISOSCELES TRAPEZOID ===");
-IsoscelesTrapezoid isoTrap =
-    new IsoscelesTrapezoid(10, 6, 4, 0, 0);
-
-Console.WriteLine($"Area = {isoTrap.Area()}");
-Console.WriteLine($"Cx = {isoTrap.CentroidX()}");
-Console.WriteLine($"Cy = {isoTrap.CentroidY()}");
-Console.WriteLine($"Ix = {isoTrap.MomentOfInertiaX()}");
-Console.WriteLine($"Iy = {isoTrap.MomentOfInertiaY()}");
-Console.WriteLine($"Ixy = {isoTrap.ProductOfInertia()}");
-
-Console.WriteLine("\n=== RIGHT TRAPEZOID (RIGHT) ===");
-RightTrapezoid rightTrap =
-    new RightTrapezoid(
+CircularTube tube =
+    new CircularTube(
         10,
-        6,
-        4,
-        RightTrapezoid.VerticalSidePosition.Right,
+        5,
+        0,
+        0,
         0,
         0);
 
-Console.WriteLine($"Area = {rightTrap.Area()}");
-Console.WriteLine($"Cx = {rightTrap.CentroidX()}");
-Console.WriteLine($"Cy = {rightTrap.CentroidY()}");
-Console.WriteLine($"Ix = {rightTrap.MomentOfInertiaX()}");
-Console.WriteLine($"Iy = {rightTrap.MomentOfInertiaY()}");
-Console.WriteLine($"Ixy = {rightTrap.ProductOfInertia()}");
-
-Console.WriteLine("\n=== RIGHT TRAPEZOID (LEFT) ===");
-RightTrapezoid leftTrap =
-    new RightTrapezoid(
-        10,
-        6,
-        4,
-        RightTrapezoid.VerticalSidePosition.Left,
-        0,
-        0);
-
-Console.WriteLine($"Area = {leftTrap.Area()}");
-Console.WriteLine($"Cx = {leftTrap.CentroidX()}");
-Console.WriteLine($"Cy = {leftTrap.CentroidY()}");
-Console.WriteLine($"Ix = {leftTrap.MomentOfInertiaX()}");
-Console.WriteLine($"Iy = {leftTrap.MomentOfInertiaY()}");
-Console.WriteLine($"Ixy = {leftTrap.ProductOfInertia()}");
-
-Console.WriteLine("\n=== CIRCLE ===");
-Circle circle = new Circle(10, 0, 0);
-
-Console.WriteLine($"Area = {circle.Area()}");
-Console.WriteLine($"Cx = {circle.CentroidX()}");
-Console.WriteLine($"Cy = {circle.CentroidY()}");
-Console.WriteLine($"Ix = {circle.MomentOfInertiaX()}");
-Console.WriteLine($"Iy = {circle.MomentOfInertiaY()}");
-Console.WriteLine($"Ixy = {circle.ProductOfInertia()}");
-
-Console.WriteLine("\n=== SEMICIRCLE ===");
-SemiCircle semiCircle = new SemiCircle(10, 0, 0);
-
-Console.WriteLine($"Area = {semiCircle.Area()}");
-Console.WriteLine($"Cx = {semiCircle.CentroidX()}");
-Console.WriteLine($"Cy = {semiCircle.CentroidY()}");
-Console.WriteLine($"Ix = {semiCircle.MomentOfInertiaX()}");
-Console.WriteLine($"Iy = {semiCircle.MomentOfInertiaY()}");
-Console.WriteLine($"Ixy = {semiCircle.ProductOfInertia()}");
+Console.WriteLine($"Area = {tube.Area()}");
+Console.WriteLine($"Cx = {tube.CentroidX()}");
+Console.WriteLine($"Cy = {tube.CentroidY()}");
+Console.WriteLine($"Ix = {tube.MomentOfInertiaX()}");
+Console.WriteLine($"Iy = {tube.MomentOfInertiaY()}");
+Console.WriteLine($"Ixy = {tube.ProductOfInertia()}");
---
+++ StructuralCalculation/Sections/HollowSections/CircularTube.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StructuralCalculation.Sections.CircularSections;
+namespace StructuralCalculation.Sections.HollowSections;
+
+public class CircularTube : Section
+{
+    public double LargerRadius { get; }
+    public double LargerOffsetX { get; }
+    public double LargerOffsetY { get; }
+    public double SmallerRadius { get; }
+    public double SmallerOffsetX { get; }
+    public double SmallerOffsetY { get; }
+    public Circle LargerCircle { get; }
+    public Circle SmallerCircle { get; }
+
+    public CircularTube(double largerRadius, double smallerRadius, double largerOffsetX, double largerOffsetY, double smallerOffsetX, double smallerOffsetY)
+    {
+        if (largerRadius <= 0 || smallerRadius <= 0)
+            throw new ArgumentException("Radius must be positive.");
+
+        if (smallerRadius >= largerRadius)
+            throw new ArgumentException("Inner radius must be smaller than outer radius.");
+
+        double distanceCenter =
+            Math.Sqrt(Math.Pow(largerOffsetX - smallerOffsetX, 2)+Math.Pow(largerOffsetY - smallerOffsetY, 2));
+
+        if (distanceCenter + smallerRadius > largerRadius)
+            throw new ArgumentException("The inner circle must be completely inside the outer circle.");
+
+        LargerRadius = largerRadius;
+        SmallerRadius = smallerRadius;
+        LargerOffsetX = largerOffsetX;
+        LargerOffsetY = largerOffsetY;
+        SmallerOffsetX = smallerOffsetX;
+        SmallerOffsetY = smallerOffsetY;
+
+        LargerCircle = new Circle(LargerRadius, LargerOffsetX, LargerOffsetY);
+        SmallerCircle = new Circle(SmallerRadius, SmallerOffsetX, SmallerOffsetY);
+
+    }
+
+    public override double Area()
+    {
+        return LargerCircle.Area() - SmallerCircle.Area();
+    }
+
+    public override double CentroidX()
+    {
+        return (LargerCircle.Area() * LargerCircle.CentroidX() - SmallerCircle.Area() * SmallerCircle.CentroidX()) / (LargerCircle.Area() - SmallerCircle.Area());
+    }
+
+    public override double CentroidY()
+    {
+        return (LargerCircle.Area() * LargerCircle.CentroidY() - SmallerCircle.Area() * SmallerCircle.CentroidY()) / (LargerCircle.Area() - SmallerCircle.Area());
+    }
+    public override double MomentOfInertiaX()
+    {
+        double centroidY = CentroidY();
+        double centroitYLargerCircle = LargerCircle.CentroidY();
+        double centroidYSmallerCircle = SmallerCircle.CentroidY();
+        double dE = centroitYLargerCircle - centroidY;
+        double dI = centroidYSmallerCircle - centroidY;
+
+        double firstTerm = LargerCircle.MomentOfInertiaX() + LargerCircle.Area() * dE * dE;
+        double secondTerm = SmallerCircle.MomentOfInertiaX() + SmallerCircle.Area() * dI * dI;
+        return firstTerm - secondTerm;
+    }
+    public override double MomentOfInertiaY()
+    {
+        double centroidX = CentroidX();
+        double centroitXLargerCircle = LargerCircle.CentroidX();
+        double centroidXSmallerCircle = SmallerCircle.CentroidX();
+        double dE = centroitXLargerCircle - centroidX;
+        double dI = centroidXSmallerCircle - centroidX;
+
+        double firstTerm = LargerCircle.MomentOfInertiaY() + LargerCircle.Area() * dE * dE;
+        double secondTerm = SmallerCircle.MomentOfInertiaY() + SmallerCircle.Area() * dI * dI;
+        return firstTerm - secondTerm;
+    }
+
+    public override double ProductOfInertia()
+    {
+        double centroidX = CentroidX();
+        double centroidY = CentroidY();
+
+        double dxE = LargerCircle.CentroidX() - centroidX;
+        double dyE = LargerCircle.CentroidY() - centroidY;
+
+        double dxI = SmallerCircle.CentroidX() - centroidX;
+        double dyI = SmallerCircle.CentroidY() - centroidY;
+
+        double firstTerm =
+            LargerCircle.ProductOfInertia()
+            + LargerCircle.Area() * dxE * dyE;
+
+        double secondTerm =
+            SmallerCircle.ProductOfInertia()
+            + SmallerCircle.Area() * dxI * dyI;
+
+        return firstTerm - secondTerm;
+    }
+}
