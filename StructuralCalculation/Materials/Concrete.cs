04540960a66292fd2f87dc9f2fc487485b0777b8
Emio Jhosef <emiojhs@gmail.com>
2026-06-18 20:48:43 -0300

    feat: add Concrete material based on NBR 6118

---
+++ StructuralCalculation/Materials/Concrete.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StructuralCalculation.Materials;
+
+namespace StructuralCalculation.Materials;
+
+//This project was calculation as NBR 6118, Brazilian.
+public class Concrete : Material
+{
+    public double Fck {  get; }
+
+    public Concrete (double fck):base(5600*Math.Sqrt(fck)*1e6, 0.2, 2500)
+    {
+        if (fck <= 0)
+            throw new ArgumentException("Fck must be positive.");
+        Fck = fck;
+    }
+}
