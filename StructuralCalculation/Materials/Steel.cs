2dfe4ea6b431eb1bc1bde0a1dedcca58d4b5707b
Emio Jhosef <emiojhs@gmail.com>
2026-06-22 21:36:24 -0300

    feat: add Steel material

---
+++ StructuralCalculation/Materials/Steel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StructuralCalculation.Materials;
+
+namespace StructuralCalculation.Materials;
+
+public class Steel : Material
+{
+    public Steel() : base(200*1e9, 0.3, 7850)
+    {
+
+    }
+}
