10c7eeaa63f011e33d9ae10a1dba71788d8c86a1
Emio Jhosef <emiojhs@gmail.com>
2026-06-22 22:17:00 -0300

    feat: add Support base class

---
+++ StructuralCalculation/Supports/Support.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralCalculation.Supports;
+
+public abstract class Support
+{
+    public bool Vertical { get; }
+    public bool Horizontal { get; }
+    public bool Moment { get; }
+    protected Support(bool vertical, bool horizontal, bool moment)
+    {
+        Vertical = vertical; 
+        Horizontal = horizontal; 
+        Moment = moment;
+    }
+
+}
