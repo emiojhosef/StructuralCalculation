64bdade7615c4b3f7b543af5bee21a646eb87e00
Emio Jhosef <emiojhs@gmail.com>
2026-06-26 20:17:40 -0300

    feat: add Node2D class

---
+++ StructuralCalculation/Nodes/Node2D.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StructuralCalculation.Supports;
+
+namespace StructuralCalculation.Nodes;
+
+
+public struct Node2D
+{
+    public double X { get; }
+    public double Y { get; }
+    public Support Node { get; }
+    public Node2D(double x, double y, Support node)
+    {
+        X= x;
+        Y= y;
+        Node = node;
+    }
+}
